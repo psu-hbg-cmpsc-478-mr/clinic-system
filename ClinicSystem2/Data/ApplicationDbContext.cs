2339f5e470f19a9eef551f116bbba60b017a15fe
Matthew A. Robinson <omegachysis@outlook.com>
2019-11-13 07:34:47 -0500

    Add Staff and Doctor roles on startup to the database if they do not already exist

--- ClinicSystem2/Data/ApplicationDbContext.cs
+++ ClinicSystem2/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ClinicSystem2.Models;
+using System.Linq;
 
 namespace ClinicSystem2.Data
 {
@@ -12,6 +13,11 @@ namespace ClinicSystem2.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            if (!Roles.Where(x => x.Name == "Staff").Any())
+                Roles.Add(new Microsoft.AspNetCore.Identity.IdentityRole("Staff"));
+            if (!Roles.Where(x => x.Name == "Doctor").Any())
+                Roles.Add(new Microsoft.AspNetCore.Identity.IdentityRole("Doctor"));
+            SaveChanges();
         }
         public DbSet<ClinicSystem2.Models.Appointment> Appointment { get; set; }
     }
